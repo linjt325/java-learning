7ac0e91c1ff3774867b93d974371e16245ce6ae1
linjt325 <249551254@qq.com>
2018-02-04 11:41:06 +0800

    mapper

---
+++ src/main/java/top/linjt/java_learning/mybatis/weChat/mapper/MessageMapper.java
@@ -0,0 +1,18 @@
+package top.linjt.java_learning.mybatis.weChat.mapper;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import top.linjt.java_learning.mybatis.weChat.pojo.MessageBean;
+
+public interface MessageMapper {
+
+		MessageBean get(int id );
+		
+		List<MessageBean> list();
+		
+		List<MessageBean> query(@Param("command")String command,@Param("description")String description) throws SQLException;
+
+}
