554b920036f4b0cf573146895d38bc03393a66cd
linjt325 <249551254@qq.com>
2018-03-04 18:39:04 +0800

    es+spring -java8

--- src/main/java/top/linjt/java_learning/elasticSearch/entity/Book.java
+++ src/main/java/top/linjt/java_learning/elasticSearch/entity/Book.java
@@ -1,9 +1,72 @@
 package top.linjt.java_learning.elasticSearch.entity;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
 /**
  * @author: XxX
  * @date: 2018/3/2
- * @Description:
+ * @Description: es book 索引
  */
 public class Book {
+    private String id;
+
+    private int price;
+
+    private  String title;
+
+    private  int count ;
+
+    private  String author;
+
+    private Timestamp updateDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Timestamp getUpdateDate() {
+        return updateDate;
+    }
+    public void setUpdateDate(Timestamp updateDate) {
+        this.updateDate = updateDate;
+    }
 }
